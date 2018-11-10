78ba33dccb5867a25916a0cc6352c3b74c3f39da
Wilson Lin <contact@wilsonl.in>
2018-11-11 02:35:01 +1300

    Fancy search term toggles

--- static/script.js
+++ static/script.js
@@ -11,8 +11,23 @@
     $search_terms.appendChild($template_search_term.content.cloneNode(true));
   });
 
-  for (const $select of $$("[data-value]")) {
+  for (const $select of $$("select[data-value]")) {
     $select.value = $select.dataset.value;
+  }
+
+  for (const $toggle of $$(".toggle")) {
+    const input = document.createElement("input");
+    // Don't use checkbox as it won't send anything if unchecked
+    input.type = "hidden";
+    input.name = $toggle.dataset.name;
+    input.value = $toggle.dataset.checked || false;
+    // Since this is initial run, only check initially if data-checked provided as "true"
+    $toggle.classList.toggle("checked", input.value === "true");
+    $toggle.appendChild(input);
+    $toggle.addEventListener("click", () => {
+      $toggle.classList.toggle("checked");
+      input.value = $toggle.classList.contains("checked");
+    });
   }
 
   Sortable.create($search_terms, {
