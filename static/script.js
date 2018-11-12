d27c50f7e613adcb27d442c14ceab30ae91372c2
Wilson Lin <contact@wilsonl.in>
2018-11-12 17:02:10 +1300

    Simplify toggle logic and fix dynamically created toggles

--- static/script.js
+++ static/script.js
@@ -14,30 +14,41 @@
     $pane.classList.remove("pane-open");
   });
 
+  const init_toggle = $toggle => {
+    const $input = document.createElement("input");
+    $toggle.appendChild($input);
+    // Don't use checkbox as it won't send anything if unchecked
+    $input.type = "hidden";
+    $input.name = $toggle.dataset.name;
+    set_toggle($toggle, $toggle.dataset.checked);
+    $toggle.addEventListener("click", () => {
+      set_toggle($toggle, !$toggle.classList.contains("checked"));
+    });
+  };
+
+  const set_toggle = ($toggle, state) => {
+    const $input = $toggle.children[0];
+    $input.value = state || false;
+    $toggle.classList.toggle("checked", $input.value === "true");
+  };
+
+  for (const $toggle of $$(".toggle")) {
+    init_toggle($toggle);
+  }
+
   const $search_terms = $("#search-terms");
   const $search_terms_add_button = $("#search-terms-add-button");
   const $template_search_term = $("#template-search-term");
   $search_terms_add_button.addEventListener("click", () => {
-    $search_terms.appendChild($template_search_term.content.cloneNode(true));
+    const $new = $template_search_term.content.cloneNode(true);
+    for (const $toggle of $$(".toggle", $new)) {
+      init_toggle($toggle);
+    }
+    $search_terms.appendChild($new);
   });
 
   for (const $select of $$("select[data-value]")) {
     $select.value = $select.dataset.value;
-  }
-
-  for (const $toggle of $$(".toggle")) {
-    const input = document.createElement("input");
-    // Don't use checkbox as it won't send anything if unchecked
-    input.type = "hidden";
-    input.name = $toggle.dataset.name;
-    input.value = $toggle.dataset.checked || false;
-    // Since this is initial run, only check initially if data-checked provided as "true"
-    $toggle.classList.toggle("checked", input.value === "true");
-    $toggle.appendChild(input);
-    $toggle.addEventListener("click", () => {
-      $toggle.classList.toggle("checked");
-      input.value = $toggle.classList.contains("checked");
-    });
   }
 
   for (const $button of $$(".search-term-button")) {
