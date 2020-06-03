2f99a8ecaa9beb731a7d0471dd36fc76e7544321
Wilson Lin <code@wilsonl.in>
2020-06-04 01:30:05 +1000

    [jobs] Search all fields

--- demo/jobs/client/src/App.tsx
+++ demo/jobs/client/src/App.tsx
@@ -3,52 +3,83 @@ import {OomlClass, ViewTemplate} from 'ooml';
 import styles from './App.css';
 
 type EdgesearchResult = {
-  id: string;
-  url: string;
-  title: string;
+  company: string;
   date: string;
+  description: string;
+  id: string;
   location: string;
   preview: string;
-  description: string;
+  title: string;
+  url: string;
 };
 
 const client = new Edgesearch.Client<EdgesearchResult>('https://jobs.wlin.workers.dev');
 
 @OomlClass
 class Result {
-  id: string = '';
-  url: string = '';
-  title: string = '';
+  company: string = '';
   date: string = '';
+  description: string = '';
+  id: string = '';
   location: string = '';
   preview: string = '';
-  description: string = '';
+  title: string = '';
+  url: string = '';
+
+  expanded: boolean = false;
+
+  expandButtonClickHandler = () => {
+    this.expanded = true;
+  };
+
+  collapseButtonClickHandler = () => {
+    this.expanded = false;
+  };
 
   [ViewTemplate] = (
-    <div>
-      <h2>{this.title}</h2>
-      <p>{this.description}</p>
+    <div className={styles.Result}>
+      <p>{this.date} | {this.company}</p>
+      <p>{this.location}</p>
+      <h2><a href={this.url} target="_blank" rel="noopener">{this.title}</a></h2>
+      <p hidden={this.expanded}>{this.preview}</p>
+      <button hidden={this.expanded} onClick={this.expandButtonClickHandler}>More</button>
+      <p hidden={!this.expanded}>{this.description}</p>
+      <button hidden={!this.expanded} onClick={this.collapseButtonClickHandler}>Less</button>
     </div>
   );
 }
 
 @OomlClass
 export class App {
-  query: string = '';
   results: EdgesearchResult[] = [];
 
-  queryChangeHandler = (e: InputEvent) => {
-    const $input = e.target as HTMLInputElement;
-    client.search(new Edgesearch.Query().add(
-      Edgesearch.Mode.REQUIRE,
-      ...$input.value.split(/[^a-zA-Z0-9]+/).filter(t => t).map(t => `title_${t.toLowerCase()}`),
-    ))
+  formSubmitHandler = (e: Event) => {
+    e.preventDefault();
+    const $form = e.target as HTMLFormElement;
+
+    const query = new Edgesearch.Query();
+    for (const $input of $form.elements) {
+      if ($input instanceof HTMLInputElement) {
+        query.add(
+          Edgesearch.Mode.REQUIRE,
+          ...$input.value.split(/[^a-zA-Z0-9]+/).filter(t => t).map(t => `${$input.name}_${t.toLowerCase()}`),
+        );
+      }
+    }
+
+    client.search(query)
       .then(results => this.results = results.results.map(r => Object.assign(new Result(), r)));
   };
 
   [ViewTemplate] = (
-    <div className={styles.app}>
-      <input value={this.query} onChange={this.queryChangeHandler}/>
+    <div className={styles.App}>
+      <form onSubmit={this.formSubmitHandler}>
+        <input name="company" placeholder="Company"/>
+        <input name="title" placeholder="Title"/>
+        <input name="location" placeholder="Location"/>
+        <input name="description" placeholder="Description"/>
+        <button type="submit">Search</button>
+      </form>
       <div>
         {this.results}
       </div>
