190ef4ad299b9504afbe0fed6e9dd3f0efe5f0ad
Wilson Lin <code@wilsonl.in>
2020-06-04 15:29:49 +1000

    Fix paths in wiki process.py

--- demo/wiki/data/process.py
+++ demo/wiki/data/process.py
@@ -1,12 +1,15 @@
 import re
 from collections import defaultdict
 from operator import itemgetter
+from os.path import dirname, realpath
 from pathlib import Path
 from sys import argv
 
 # Use this script with monthly article page views data from https://dumps.wikimedia.org/other/pagecounts-ez/.
 
-Path('build').mkdir(exist_ok=True)
+DATA_DIR = dirname(realpath(argv[0]))
+
+Path(DATA_DIR + '/build').mkdir(exist_ok=True)
 
 titles = defaultdict(int)
 for line in open(argv[1], 'r'):
@@ -22,8 +25,8 @@ for line in open(argv[1], 'r'):
 
 sorted_titles = sorted(((title, count) for title, count in titles.items()), key=itemgetter(1), reverse=True)
 
-f_documents = open('build/docs.txt', 'w')
-f_terms = open('build/terms.txt', 'w')
+f_documents = open(DATA_DIR + '/build/docs.txt', 'w')
+f_terms = open(DATA_DIR + '/build/terms.txt', 'w')
 
 terms_regex = re.compile('[^a-zA-Z0-9]+')
 for (title, _) in sorted_titles:
@@ -37,5 +40,5 @@ for (title, _) in sorted_titles:
         f_terms.write('\0')
     f_terms.write('\0')
 
-with open('build/default.json', 'w') as f:
+with open(DATA_DIR + '/build/default.json', 'w') as f:
     f.write('[]')
