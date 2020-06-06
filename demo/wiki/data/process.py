93fbe836e14ab2c91429c378d38636298400b8b7
Wilson Lin <code@wilsonl.in>
2020-06-06 16:28:13 +1000

    [wiki] Use JSON docs

--- demo/wiki/data/process.py
+++ demo/wiki/data/process.py
@@ -1,5 +1,6 @@
 import re
 from collections import defaultdict
+from json import dumps
 from operator import itemgetter
 from os.path import dirname, realpath
 from pathlib import Path
@@ -7,9 +8,12 @@ from sys import argv
 
 # Use this script with monthly article page views data from https://dumps.wikimedia.org/other/pagecounts-ez/.
 
-DATA_DIR = dirname(realpath(argv[0]))
+OUT_DIR = dirname(realpath(argv[0]))
+Path(OUT_DIR + '/build').mkdir(exist_ok=True)
 
-Path(DATA_DIR + '/build').mkdir(exist_ok=True)
+if len(argv) < 2:
+    print('Pass the path to the dataset as the first argument')
+    exit(1)
 
 titles = defaultdict(int)
 for line in open(argv[1], 'r'):
@@ -25,20 +29,20 @@ for line in open(argv[1], 'r'):
 
 sorted_titles = sorted(((title, count) for title, count in titles.items()), key=itemgetter(1), reverse=True)
 
-f_documents = open(DATA_DIR + '/build/docs.txt', 'w')
-f_terms = open(DATA_DIR + '/build/terms.txt', 'w')
+f_documents = open(OUT_DIR + '/build/docs.txt', 'w')
+f_terms = open(OUT_DIR + '/build/terms.txt', 'w')
 
 terms_regex = re.compile('[^a-zA-Z0-9]+')
 for (title, _) in sorted_titles:
     terms = {t.lower() for t in terms_regex.split(title) if t}
     if not terms:
         continue
-    f_documents.write(title)
+    f_documents.write(dumps(title))
     f_documents.write('\0')
     for term in terms:
         f_terms.write(term)
         f_terms.write('\0')
     f_terms.write('\0')
 
-with open(DATA_DIR + '/build/default.json', 'w') as f:
+with open(OUT_DIR + '/build/default.json', 'w') as f:
     f.write('[]')
