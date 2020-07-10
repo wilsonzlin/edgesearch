043e7dd14cd4f4857103a55fdbb520bbb6b8244d
Wilson Lin <code@wilsonl.in>
2020-07-10 11:10:48 +1000

    [jobs] Fix company property in data

--- demo/jobs/data/get-data.js
+++ demo/jobs/data/get-data.js
@@ -14,7 +14,8 @@ const FIELDS = ['company', 'description', 'location', 'title'];
     cacheDir: path.join(__dirname, 'cache'),
     companies: COMPANIES,
   });
-  const rawJobsSorted = COMPANIES.flatMap(c => rawJobs[c]).sort((a, b) => (b.date || '').localeCompare(a.date || ''));
+  const rawJobsSorted = COMPANIES.flatMap(c => rawJobs[c].map(j => ({...j, company: c})))
+    .sort((a, b) => (b.date || '').localeCompare(a.date || ''));
 
   await fs.promises.writeFile(path.join(BUILD, 'jobs.json'), JSON.stringify(rawJobs));
 
@@ -22,7 +23,6 @@ const FIELDS = ['company', 'description', 'location', 'title'];
   const terms = [];
 
   for (const job of rawJobsSorted) {
-    job.company = company;
     documents.push(JSON.stringify(job), '\0');
     for (const field of FIELDS) {
       for (const word of (job[field] || '').split(/[^a-zA-Z0-9]+/).filter(w => w).map(w => w.toLowerCase())) {
